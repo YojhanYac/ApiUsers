cf73170fff044020728825ed39a7f758cf72d263
yojhanyac <yac@live.com.ar>
2020-09-15 02:06:48 -0300

    update!

--- public/index.php
+++ public/index.php
@@ -1,40 +1,38 @@
 <?php
 
 // Creación de una RESTFUL API o API REST con slim framework (PHP, MySql, PDO)
-// DominiCode:                      https://www.youtube.com/watch?v=iLRjbGC6jIs&ab_channel=DominiCode
 // POSTMAN, XAMP, MySQL Workbench, GIT Bash
-// Slim 4 Documentation:            http://www.slimframework.com/docs/v4
-// Composer:                        https://getcomposer.org/download/
-// Slim v4                          composer require slim/slim:"4.*"
-// Slim PSR-7:                      composer require slim/psr7
+
+// DominiCode:                          https://www.youtube.com/watch?v=iLRjbGC6jIs&ab_channel=DominiCode
+// Slim 4 Documentation:                http://www.slimframework.com/docs/v4
+// Composer:                            https://getcomposer.org/download/
+// Slim v4                              composer require slim/slim:"4.*"
+// Slim PSR-7:                          composer require slim/psr7
 
 // API URLs:
-// Mostrar clientes:                http://localhost/API_Usuarios/public/api/clientes
+// GET Mostrar clientes:                http://localhost/API_Usuarios/public/api/clientes
 
-// Mostrar cliente por id:          http://localhost/API_Usuarios/public/api/clientes/{id}
+// GET Mostrar cliente por id:          http://localhost/API_Usuarios/public/api/clientes/{id}
 
-/* Agregar cliente nuevo:           http://localhost/API_Usuarios/public/api/clientes/nuevo
-    POST
-        Headers: KEY: Content-Type  VALUE: application/json
-        BODY: row JSON  {
+/* POST Agregar cliente nuevo:          http://localhost/API_Usuarios/public/api/clientes/nuevo
+    Headers: KEY: Content-Type  VALUE: application/json
+        Body: row JSON  {
                             "name": "NOMBRE",
                             "email": "CORREO@MAIL.COM",
                             "password": "CONTRASEÑA"
                         }
 */
 
-/* Modificar cliente por id:        http://localhost/API_Usuarios/public/api/clientes/modificar/{id} 
-    PUT
-        Headers: KEY: Content-Type  VALUE: application/json
-        BODY: row JSON  {
+/* PUT Modificar cliente por id:        http://localhost/API_Usuarios/public/api/clientes/modificar/{id} 
+    Headers: KEY: Content-Type  VALUE: application/json
+        Body: row JSON  {
                             "name": "NUEVO_NOMBRE",
                             "email": "NUEVO_CORREO@MAIL.COM",
                             "password": "NUEVA_CONTRASEÑA"
                         }
 */
 
-// Eliminar cliente por id:         http://localhost/API_Usuarios/public/api/clientes/delete/{id}
-
+// DELETE Eliminar cliente por id:      http://localhost/API_Usuarios/public/api/clientes/delete/{id}
 
 /*
     CREATE DATABASE db_apiusers;
